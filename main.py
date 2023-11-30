a4c3ac55375bc38ab4a2c37fd13cf2fd332c6cc1
Davide Arcolini <davidearcolini.dev@gmail.com>
2023-11-30 15:40:13 +0100

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+# buggy_code.py
+
+# Bug 1: Syntax Error
+print("Hello, world!"
+
+# Bug 2: Index Error
+numbers = [1, 2, 3, 4, 5]
+print(numbers[5])
+
+# Bug 3: NameError
+result = x + 5
+print(result)
