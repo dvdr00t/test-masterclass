9525adc75612eaa460f89a358d8ed46665c454f5
edoardocolella <edoardocolella.it@gmail.com>
2023-11-30 22:14:36 +0100

    test

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ from main import *
 
 
 def test():
-    print(fun1()==1)
-    print(fun2()==2)
-    print(fun3()==3)
+    print(1, fun1()==1)
+    print(2, fun2()==2)
+    print(3, fun3()==3)
 
 
 if __name__ == '__main__':
